8859721b79950dffb23db6cba48987d7d31913c3
WeiChengTseng <weichengtseng0731@gmail.com>
2016-02-11 20:34:34 -0600

    first_textured_square_FEB_11

--- openGL_the_GLFW_1/Source.cpp
+++ openGL_the_GLFW_1/Source.cpp
@@ -2,9 +2,11 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "Shader.h"
+#include "SOIL.h"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
+
 
 /*function prototype*/
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -38,14 +40,37 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 
 	/*define vertex for triangle*/
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	
-		0.0f, 0.5f, 0.0f,		0.0f, 0.0f, 1.0f
+	GLfloat vertices[] = {
+		// Positions          // Colors           // Texture Coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Bottom Left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Top Left 
 	};
 
-	/*define transformation*/
+	GLuint indices[] = {  // Note that we start from 0!
+		0, 1, 3,  // First Triangle
+		1, 2, 3   // Second Triangle
+	};
+
+
+	/*define texture*/
+	int width, height;
+	unsigned char* image = SOIL_load_image("C:/Users/Wei-Cheng/Documents/Visual Studio 2015/Projects/openGL_the_GLFW_1/container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+
+	/*Generate Texture*/
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	
+
+
+	/*define and test on transformation*/
 	glm::vec4 vec(1.0f,0.0f,0.0f,1.0f);
 	glm::mat4 trans;
 	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f,0.0f));
@@ -62,17 +87,27 @@ int main()
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	
+
+	/*EBO*/
+	GLuint EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices, GL_STATIC_DRAW);
+
 	
 	/*link the shader with input*/
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	/*enable vertex attribute array*/
 	glEnableVertexAttribArray(0);
 	
 	/*link the shader with input*/
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	/*enable vertex attribute array*/
 	glEnableVertexAttribArray(1);
 
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
@@ -90,12 +125,11 @@ int main()
 		
 		/*use program*/
 		shaderProgram.use();
-		
 
-
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
