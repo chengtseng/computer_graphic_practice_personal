868a2a3d9486d9775f268ac966720faa694b09db
WeiChengTseng <weichengtseng0731@gmail.com>
2016-02-14 12:19:46 -0600

    draw_rotating_box_with_VAO_only_FEB_14

--- openGL_the_GLFW_1/Source.cpp
+++ openGL_the_GLFW_1/Source.cpp
@@ -43,21 +43,51 @@ int main()
 	glfwSetKeyCallback(window, key_callback);
 
 	/*define vertex for triangle*/
-	GLfloat vertices[] = {
-		// Positions          // Colors          // Texture Coords
-		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Top Right
-		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Bottom Right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,  // Bottom Left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f   // Top Left 
-	};
+	GLfloat vertices[] = 
+	{
+		// Positions                   // Texture Coords
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,		1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f,		1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f,		1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f,		0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,	
 
-	/*drawing order*/
-	GLuint indices[] = 
-	{  
-		0, 1, 3,  // First Triangle
-		1, 2, 3   // Second Triangle
-	};
+		-0.5f, -0.5f, 0.5f,		0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f,		1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f,		0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f,		0.0f, 0.0f,
 
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f,		1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f,		0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+
+		0.5f, -0.5f, -0.5f,		0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f,		1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f,		0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f,		0.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f,		1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f,		1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f,		1.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f,		0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+
+		-0.5f, 0.5f, -0.5f,		0.0f, 0.0f,
+		0.5f, 0.5f, -0.5f,		1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f,		1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f,		0.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f,		0.0f, 0.0f,
+	};
 
 	/*define texture*/	
 	GLuint texture1, texture2;
@@ -82,47 +112,23 @@ int main()
 	SOIL_free_image_data(image2);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
-	/*unrelated to our code but define and test on transformation*/
-	//glm::vec4 vec(1.0f,0.0f,0.0f,1.0f);
-	//glm::mat4 trans;
-	//trans = glm::translate(trans, glm::vec3(1.0f, 1.0f,0.0f));
-	//vec = trans * vec;
-	//std::cout << vec.x << vec.y << vec.z << std::endl;
-
-	/*VAO*/
-	GLuint VAO;
+	/*Array Buffer*/
+	GLuint VBO, VAO;
 	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	/*VBO*/
-	GLuint VBO;
 	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	
 
-	/*EBO*/
-	GLuint EBO;
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices),indices, GL_STATIC_DRAW);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER,VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	
-	/*link the shader with input*/
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-	/*enable vertex attribute array*/
+	/*assign position attribute in shader*/
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5* sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	
-	/*link the shader with input*/
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	/*enable vertex attribute array*/
-	glEnableVertexAttribArray(1);
 
-	/*link the shader with vertex coordinate*/
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-	/*enable vertex attribute array*/
+	/*assign texCoord attribute in shader*/
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
 	glEnableVertexAttribArray(2);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
 	/*import and link our shader program*/
@@ -141,7 +147,7 @@ int main()
 		/*Create transformation matrix every time during interation*/
 		glm::mat4 trans;		
 		trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-		trans = glm::rotate(trans, 10*(GLfloat)glfwGetTime()*glm::radians(90.0f), glm::vec3(0.0, 0.2, 0.0));
+		trans = glm::rotate(trans, (GLfloat)glfwGetTime()*glm::radians(90.0f), glm::vec3(0.0, 0.2, 0.0));
 		GLuint transformLoc = glGetUniformLocation(shaderProgram.program,"transform");
 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
@@ -155,7 +161,7 @@ int main()
 		
 		/*bind vertex array*/
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES,0,36);
 		glBindVertexArray(0);
 
 		/*window swapping*/
@@ -165,7 +171,6 @@ int main()
 	/*house keeping*/
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
 }
