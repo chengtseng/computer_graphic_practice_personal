9257817cf2cb8babd5595f938eefc404b6408685
WeiChengTseng <weichengtseng0731@gmail.com>
2016-02-14 19:19:30 -0600

    camera_roation_Feb_14

--- openGL_the_GLFW_1/Source.cpp
+++ openGL_the_GLFW_1/Source.cpp
@@ -167,20 +167,24 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 
-		//
+		/*define matrices*/
 		glm::mat4 model;
 		model = glm::rotate(model, (GLfloat)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
-		glm::mat4 view;
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//glm::mat4 view;
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-		glm::mat4 projection;
-		projection = glm::perspective(glm::radians(45.0f), (float)win_width / win_height, 0.1f, 100.0f);
+		GLfloat radius =10.0f;
+		GLfloat camX = sin(glfwGetTime())*radius;
+		GLfloat camZ = cos(glfwGetTime()) * radius;
+		view = glm::lookAt(glm::vec3(camZ, 0.0, camX), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+		
+	    projection = glm::perspective(glm::radians(45.0f), (float)win_width / win_height, 0.1f, 100.0f);
 		
 		/*use program*/
 		shaderProgram.use();
 
-		/*Create transformation matrix every time during interation*/
+		
 		
 		GLuint modelLoc = glGetUniformLocation(shaderProgram.program,"model");
 		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
